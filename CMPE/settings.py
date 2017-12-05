acafac467501e83367ae9c8fb27d53b851e2962a
Ordo <matt@spadarolabs.com>
2017-12-04 18:44:48 -0800

    Added files and changes required for deployment to heroku

--- CMPE/settings.py
+++ CMPE/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'CMPE.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
