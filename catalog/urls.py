a25e69ae866c731fd7683725eb4513c28a44af59
Ordo <matt@spadarolabs.com>
2017-12-11 13:14:43 -0800

    final touches for version 1.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
  	url(r'^$', views.index, name='index'),
- 	url(r'^games/$', views.GamesListView.as_view(), name='games'),
+ 	url(r'^games/$', views.GamesListView, name='games'),
  	url(r'^game/(?P<pk>\d+)$', views.GameDetailView.as_view(), name='game-detail'),
  	url(r'^signup/$', views.signup, name='signup'),
  	url(r'^game/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
